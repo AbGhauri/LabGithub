de4ae953a8d893db780b95a16ef0dce4a816d895
adilryk <adilshafiqryk007@gmail.com>
2024-09-27 23:01:20 +0500

    final commit

--- Product_stubs.py
+++ Product_stubs.py
@@ -5,8 +5,28 @@ class Product:
       self.price = price
 
   def get_price(self, quantity):
-      pass
+        # sp23-bai-003
+        try:
+            if quantity <= 0:
+                raise ValueError("Number to be bought must be a positive integer.")
+            
+            # Calculate the discount
+            discount = 0
+            if quantity < 10:
+                pass 
+            elif 10 <= quantity < 99:
+                discount = 10  # 10% discount
+            else:
+                discount = 20  # 20% discount
 
+            # Calculate final price 
+            price = (100 - discount) / 100 * self.price
+            return price * quantity
+
+        except ValueError as e:
+            print(f"Error occurred: {e}")
+            return None  
+#sp23-bai-002
   def make_purchase(self, quantity):
       pass
 
